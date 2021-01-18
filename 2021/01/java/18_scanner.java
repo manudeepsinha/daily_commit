539139a399f3cb5ed05cf9580fe63e7536fdcaa8
Manudeep Sinha <40653329+manudeepsinha@users.noreply.github.com>
2021-01-18 23:41:00 +0530

    Add files via upload

---
+++ 2021/01/java/18_scanner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner ;
+
+public class test{
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.readLine();
+		System.out.println("String is: " + s);
+
+		int i = sc.nextInt();
+		System.out.println("Integer is: " + i);
+
+		float f = sc.nextFloat();
+		System.out.println("Float is: " + f);
+
+	}
+}
