4d34a83dd64937001863b50ffd92dd2c70360199
Manudeep Sinha <40653329+manudeepsinha@users.noreply.github.com>
2021-01-18 23:41:15 +0530

    Add files via upload

---
+++ 2021/01/java/18_format.java
@@ -0,0 +1,12 @@
+import java.util.Scanner ;
+
+public class test{
+	public static void main(String [] args) {
+		float pi = Math.PI;
+		System.out.format("Value of pi: %f\n",pi);
+		System.out.format("Value of pi: %.2f\n",pi);
+		System.out.format("Value of pi: %6.2f\n",pi);
+		System.out.printf("Value of pi: %06.2f\n",pi);
+
+	}
+}
